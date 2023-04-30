c465b90811dff5beae0447986fc545381ee57609
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-05-01 00:32:36 +0530

    fix: Bug fixed in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Typed from "react-typed";
 
 const Hero = () => {
   return (
@@ -14,15 +13,8 @@ const Hero = () => {
 
         <div className="flex justify-center items-center">
           <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
-            Fast, flexible financing for{" "}
+            Fast, flexible financing for BTB, BTC & SASS
           </p>
-          <Typed
-            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-            strings={["BTB", "BTC", "SASS"]}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
         </div>
 
         <p className="md:text-2xl text-xl font-bold text-gray-500">
